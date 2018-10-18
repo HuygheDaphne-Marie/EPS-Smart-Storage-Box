24d8357699405e493d72aa99dd86647876a8d009
Henri-Thibault Huyghe <henrithibault.huyghe@gmail.com>
2018-10-18 23:21:35 +0200

    Request front end work

---
+++ public/js/inventory.js
@@ -0,0 +1,39 @@
+const $btn = document.querySelector('#rTest');
+const $list = document.querySelector('#requestList');
+const $addBtn = document.querySelectorAll('.list-add')
+
+let request = []
+
+function showRequest() {
+  let HTMLstr = '';
+  request.forEach(item => {
+    HTMLstr += `<li class="collection-item">${item.UID}: ${item.amount}</li>`
+  })
+  $list.innerHTML = HTMLstr;
+}
+
+
+$addBtn.forEach(btn => {
+  btn.addEventListener('click', event => {
+    const item = request.find(itemRequest => {
+      return itemRequest.UID === btn.getAttribute('data-UID');
+    })
+
+    if(item !== undefined) {
+      item.amount++;
+    } else {
+      request.push({UID: btn.getAttribute('data-UID'), amount: 1})
+    }
+
+    showRequest();
+  })
+})
+
+
+$btn.addEventListener('click', event => {
+  console.log('TRIG')
+  $addBtn.forEach(btn => {
+    btn.classList.toggle('active');
+  });
+  $list.classList.toggle('active');
+});
