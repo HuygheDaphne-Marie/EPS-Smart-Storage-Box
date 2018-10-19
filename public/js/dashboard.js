b0a53a43b5aaccbf43389f4f487aa723dda998db
Henri-Thibault Huyghe <henrithibault.huyghe@gmail.com>
2018-10-19 21:54:31 +0200

    Playing around with more chart types

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,13 +1,15 @@
 // Chart options
 const $btn = document.querySelector('#gen');
 let chartData = [];
+let c3Data = [];
+
 const options = {
   chart: {
-    height: 450,
-    width: '50%',
     type: 'bar',
     background: '#f4f4f4',
-    foreColor: '#333'
+    foreColor: '#333',
+    height: 350,
+    // width: '69%'
   },
   series: [{
     name: 'Number',
@@ -37,15 +39,89 @@ const options = {
     }
   }
 };
+const options1 = {
+  chart: {
+    type: 'radialBar',
+    background: '#f4f4f4',
+    foreColor: '#333',
+    height: 350
+  },
+  series: [44, 55, 67, 83],
+  plotOptions: {
+    circle: {
+      dataLabels: {
+          showOn: 'hover'
+      }
+    }
+  },
+  labels: ['Apples', 'Oranges', 'Bananas', 'Berries'],
+  dataLabels: {
+    enabled: false
+  }
+};
+const options2 = {
+  chart: {
+    type: 'donut',
+    background: '#f4f4f4',
+    foreColor: '#333',
+    height: 350
+  },
+  series: [44, 25, 49],
+  labels: ['Apples', 'Oranges', 'Bananas'],
+  dataLabels: {
+    enabled: false
+  }
+};
+const options3 = {
+  chart: {
+    type: 'bar',
+    background: '#f4f4f4',
+    foreColor: '#333',
+    height: 350
+  },
+  series: [{
+    name: 'Number',
+    data: c3Data
+  }],
+  xaxis: {
+    categories: ['Label1', 'Label2', 'Label3', 'Label4', 'Label5', 'Label6']
+  },
+  plotOptions: {
+    bar: {
+      horizontal: false,
+    }
+  },
+  fill: {
+    colors: ['#009688']
+  },
+  dataLabels: {
+    enabled: false
+  },
+  title: {
+    text: 'Some Random Numbers',
+    align: 'center',
+    margin: 20,
+    offsetY: 20,
+    style: {
+      fontSize: '25px'
+    }
+  }
+};
 
 // Init chart
 const chart = new ApexCharts(document.querySelector('#chart'), options);
+const c1 = new ApexCharts(document.querySelector('#c1'), options1);
+const c2 = new ApexCharts(document.querySelector('#c2'), options2);
+const c3 = new ApexCharts(document.querySelector('#c3'), options3);
 
 function generateRandomNum() {
   return Math.floor(Math.random()*100+1)
 }
 for(let idx = 0; idx < 15; idx++) {
   chartData.push(generateRandomNum());
+}
+for(let idx = 0; idx < 6; idx++) {
+  c3Data.push(generateRandomNum());
 }
 
 $btn.addEventListener('click', event => {
@@ -57,8 +133,18 @@ $btn.addEventListener('click', event => {
     name: 'Number',
     data: newData
   }])
+  c1.updateSeries([newData[2], newData[4], newData[5], newData[8]])
+  c2.updateSeries([newData[2], newData[4], newData[5]])
+  c3.updateSeries([{
+    name: 'Number',
+    data: [newData[2], newData[4], newData[5], newData[8], newData[9], newData[11]]
+  }])
+  
 })
 
 
 // Render chart
 chart.render();
+c1.render();
+c2.render();
+c3.render();
