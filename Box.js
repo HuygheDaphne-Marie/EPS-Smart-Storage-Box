c27b1761ac07e57918ae0f4c1fd179c96990ddc5
Huyghe Henri-Thibault <henri-thibault@student.howest.be>
2018-11-29 11:07:49 +0100

    Added basic functionality to Box & Frame

--- Box.js
+++ Box.js
@@ -1,7 +1,16 @@
+const Item = require('./Item');
+
 class Box {
   constructor(Item) {
     this.itemHeld = Item;
+  }
 
+  switchItem(newItem) {
+    if(newItem instanceof Item) {
+      this.itemHeld = newItem;
+    } else {
+      throw "Box itemHeld must be of the class Item!";
+    }
   }
 }
 
--- Frame.js
+++ Frame.js
@@ -1,6 +1,16 @@
+const Box = require('./Box');
+
 class Frame {
   constructor() {
     this.boxes = [];
+  }
+
+  addBox(box) {
+    if(box instanceof Box) {
+      this.boxes.push(box);
+    } else {
+      throw "Frame boxes must be of the class Box!";
+    }
   }
 }
 
