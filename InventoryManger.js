65dbe09d98d2ce887f004866e7c07113f8d44870
Henri-Thibault Huyghe <henrithibault.huyghe@gmail.com>
2019-01-19 13:50:09 +0100

    Commented mysql code for demo, fs way should work 100% of the time and no chance of connection flukes

--- InventoryManger.js
+++ InventoryManger.js
@@ -1,28 +1,32 @@
 const Item = require('./Item');
-const mysql = require('mysql');
+// const mysql = require('mysql');
+const fs = require('fs');
 
 class InventoryManager {
   constructor() {
-    this.connection = mysql.createConnection({
-      host: 'localhost',
-      user: 'root', // Add user
-      password: '', // Add password
-      database : 'storagebox'
-    })
+    // this.connection = mysql.createConnection({
+    //   host: 'localhost',
+    //   user: 'root', // Add user
+    //   password: '', // Add password
+    //   database : 'storagebox'
+    // })
     this.items = [];
-    this.fetchItems(this.items);
+    JSON.parse(fs.readFileSync('items.json', 'utf8')).forEach(itemdata => {
+      this.items.push(new Item(itemdata.id, itemdata.name, itemdata.stock));
+    })
+    // this.fetchItems(this.items);
   }
 
-  fetchItems(items) {
-    this.connection.connect()
-    this.connection.query('SELECT * FROM item', (err, rows, fields) => {
-      if (err) throw err
-      rows.forEach(row => {
-        items.push(new Item(row.id, row.name, row.stock))
-      })
-    })
-    this.connection.end()
-  };
+  // fetchItems(items) {
+  //   this.connection.connect()
+  //   this.connection.query('SELECT * FROM item', (err, rows, fields) => {
+  //     if (err) throw err
+  //     rows.forEach(row => {
+  //       items.push(new Item(row.id, row.name, row.stock))
+  //     })
+  //   })
+  //   this.connection.end()
+  // };
 
   orderItem(UID, amount) {
     const itemOrdered = this.items.find(item => {
